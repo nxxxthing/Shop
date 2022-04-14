c977807f5d8f1817df8e575567fc8dfd3f6ce932
Illia Volynets <illiavolynets77@gmail.com>
2022-04-14 16:55:41 +0300

    added admin page template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+//    'middleware' => 'role:admin|user',
+    'prefix' => 'admin-panel'
+], function ($router) {
+    Route::get('/', [HomeController::class, 'index']);
 });
 
 
